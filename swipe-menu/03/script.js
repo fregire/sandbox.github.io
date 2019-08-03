631bd0cd8ca06242ad8597ec49bea22ac2c1d03c
fregire <fregire@mail.ru>
2019-08-03 19:38:33 +0500

    Добавил расстояние поменьше от края до конца панели

--- swipe-menu/03/script.js
+++ swipe-menu/03/script.js
@@ -354,7 +354,7 @@ document.addEventListener("DOMContentLoaded", function(){
         elemSelector: '.sidebar',
         elementWidth: $(sidebar).width(),
         elementMaxWidth: 1, 
-        sideHookWidth: 8, 
+        sideHookWidth: 5, 
         moveSpeed: 0.4,
         opacityBackground: 0.5,
         shiftForStart: 100,
